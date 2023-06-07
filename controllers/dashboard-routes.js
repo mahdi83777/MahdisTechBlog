4c7efb685fdb5201bddc6670811f8690e911bd37
mahdi83777 <mye09@mail.aub.edu>
2023-06-07 21:52:02 +0300

    added db, config, controllers

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { Post, User } = require('../models/');
+const withAuth = require('../utils/auth');
+
+// ALL POSTS DASHBOARD
+router.get('/', withAuth, async (req, res) => {
+  try {
+    // store the results of the db query in a variable called postData
+    const postData = await Post.findAll({
+      where:{"userId": req.session.userId},
+      include: [User]
+    });
+    // this sanitizes the data we just got from the db above
+    const posts = postData.map((post) => post.get({ plain: true }));
+console.log(posts);
+    res.render('all-posts', {
+      // this is how we specify a different layout other than main
+      layout: 'dashboard',
+      posts,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+// AFTER CLICK ON NEW POST BUTTON
+router.get('/new', withAuth, (req, res) => {
+  res.render('new-post', {
+    layout: 'dashboard',
+  });
+});
+
+// WHEN WE CLICK ON THE POST ITSELF
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    //we need to get some data passed via the request body
+    const postData = await Post.findByPk(req.params.id);
+
+    if (postData) {
+      // serializing the data
+      const post = postData.get({ plain: true });
+      console.log(post);
+      res.render('edit-post', {
+        layout: 'dashboard',
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+module.exports = router;
